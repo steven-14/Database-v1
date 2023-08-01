662878bb3e0f8df2540a6ee13eb82d620bfab323
steven-14 <104389629+steven-14@users.noreply.github.com>
2023-08-01 09:14:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-require('dotenv').config();
-
 const express = require('express')
 const mongoose = require('mongoose')
-const Book = require("./models/books");
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
@@ -19,38 +15,8 @@ const connectDB = async () => {
 }
 
 //Routes go here
-app.get('/', (req,res) => {
-    res.send({ title: 'Books' });
-})
-
-app.get('/books', async (req,res)=> {
-
-  const book = await Book.find();
-
-  if (book) {
-    res.json(book)
-  } else {
-    res.send("Something went wrong.");
-  }
-  
-});
-
-app.get('/add-note', async (req,res) => {
-  try {
-    await Book.insertMany([
-      {
-        title: "Sons Of Anarchy",
-        body: "Body text goes here...",
-      },
-      {
-        title: "Games of Thrones",
-        body: "Body text goes here...",
-      }
-    ]);
-    res.json({"Data":"Added"})
-  } catch (error) {
-    console.log("err", + error);
-  }
+app.all('*', (req,res) => {
+    res.json({"every thing":"is awesome"})
 })
 
 //Connect to the database before listening
